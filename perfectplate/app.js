5adde8f2fefb5589ddb4dffe403bcd728cfae643
Lucas Ribolli <lucasribolli@gmail.com>
2021-12-04 19:33:54 -0300

    update plates query all to include ingredients list

--- perfectplate/app.js
+++ perfectplate/app.js
@@ -90,28 +90,49 @@ app.get('/ingredients/query', function (req, res, next) {
   })
 })
 
-app.get('/plates/query_all', function (req, res, next) {
-  var userId = req.query.user_id
-  db.query(
-    'SELECT'
-    + ' p.id AS plate_id,'
-    + ' p.date AS date,'
-    + ' p.name AS name,'
-    + ' pi.id AS plate_ingredients_id,'
-    + ' pi.ingredient_id AS ingredient_id,'
-    + ' pi.number_of_portions AS number_of_portions'
-    + ' FROM plates AS p'
-    + ' INNER JOIN plate_ingredients AS pi'
-    + ' ON p.id = pi.plate_id'
-    + ' AND p.user_id = $1',
-    [userId]
-  )
-  .then((result) => {
-    res.send(responses.success(result.rows))
-  })
-  .catch((err) => {
+app.get('/plates/query_all', async function (req, res, _) {
+  try {
+    var userId = req.query.user_id
+    var plateResult = await db.query(
+      'SELECT'
+      + ' p.id AS plate_id,'
+      + ' p.date AS date,'
+      + ' p.name AS name,'
+      + ' pi.ingredient_id AS ingredient_id,'
+      + ' pi.number_of_portions AS number_of_portions'
+      + ' FROM plates AS p'
+      + ' INNER JOIN plate_ingredients AS pi'
+      + ' ON p.id = pi.plate_id'
+      + ' AND p.user_id = $1',
+      [userId]
+    )
+    var plates = Array();
+    for(var i=0; i < plateResult.rowCount; i++) {
+      var plate = plateResult.rows[i]
+      var ingredientsResult = await db.query(
+        'SELECT'
+        + ' name,'
+        + ' one_portion_weight,'
+        + ' classification,'
+        + ' energetic_value,'
+        + ' carbohydrate,'
+        + ' protein,'
+        + ' saturated_fat,'
+        + ' total_fat,'
+        + ' trans_fat,'
+        + ' fibre,'
+        + ' sodium'
+        + ' FROM INGREDIENTS WHERE id = $1',
+        [plate.ingredient_id]
+      )
+      plate.ingredients = ingredientsResult.rows;
+      plates.push(plate)
+    }
+    res.send(responses.success(plates))
+  } catch (e) {
     res.send(responses.fail(err))
-  })
+  }
+  
 })
 
 app.post('/plates/plate/insert', function (req, res, next) {
