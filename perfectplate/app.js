13130a1441a26555d60d25fa92ef0b543f7a1c7e
ViniciusZacheu <vnzacheu@gmail.com>
2021-12-06 22:06:49 -0300

    feat: added userType prop to login route return

--- perfectplate/app.js
+++ perfectplate/app.js
@@ -33,12 +33,12 @@ app.post('/users/signup', function (req, res, next) {
 
   db.query(
     "INSERT INTO USERS " +
-    "(email, password, name, age, sex, weight, height, userType) " + 
-    "VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id", 
+    "(email, password, name, age, sex, weight, height, userType) " +
+    "VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id",
     [email, password, name, age, sex, weight, height, userType]
   )
   .then((result) => {
-    res.send(responses.success(result.rows[0].id)) 
+    res.send(responses.success(result.rows[0].id))
   })
   .catch((err) => {
     res.send(responses.fail(errors[err.code]))
@@ -57,7 +57,7 @@ app.post('/users/login', function (req, res, next) {
     if(result.rowCount == 0) {
       successOrFail = responses.fail("USER_UNFOUND")
     } else {
-      successOrFail = responses.success(result.rows[0].id)
+      successOrFail = responses.success({id: result.rows[0].id, userType: result.rows[0].userType})
     }
     res.send(successOrFail)
   })
@@ -132,7 +132,7 @@ app.get('/plates/query_all', async function (req, res, _) {
   } catch (e) {
     res.send(responses.fail(err))
   }
-  
+
 })
 
 app.post('/plates/plate/insert', function (req, res, next) {
